0d6889c5ec9d88c6915678ce8f01548dd2c23b03
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-08 14:51:47 -0600

    Change grid colors when mouse hovers over grid

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,9 +1,16 @@
+//Create grid
 const masterContainer = document.getElementsByClassName("master-container")[0];
 
 let count = 0;
 while (count < 16*16){
     let grid = document.createElement("div");
-    //grid.textContent = "\n";
     masterContainer.appendChild(grid);
     count ++;
 }
+
+//Change color of squares
+const nestedDivs = document.querySelectorAll(".master-container div");
+
+nestedDivs.forEach((div) => {
+    div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
+});
