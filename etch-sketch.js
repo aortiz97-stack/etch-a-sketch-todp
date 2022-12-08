8ce6888ffc72dedaa111e6cbafc249551c49a6f3
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-08 12:36:31 -0600

    Arrange nested div containers into 16x16 grid

--- etch-sketch.js
+++ etch-sketch.js
@@ -3,7 +3,7 @@ const masterContainer = document.getElementsByClassName("master-container")[0];
 let count = 0;
 while (count < 16*16){
     let grid = document.createElement("div");
-    grid.textContent = "Filler Words"
+    grid.textContent = "--Filler Words--"
     masterContainer.appendChild(grid);
     count ++;
 }
