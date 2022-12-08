51eab356cc48a2fedd82eda2093d2fe1c990b2ab
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-08 13:39:24 -0600

    Remove redundant textContent from nested divs

--- etch-sketch.js
+++ etch-sketch.js
@@ -3,7 +3,7 @@ const masterContainer = document.getElementsByClassName("master-container")[0];
 let count = 0;
 while (count < 16*16){
     let grid = document.createElement("div");
-    grid.textContent = "\n";
+    //grid.textContent = "\n";
     masterContainer.appendChild(grid);
     count ++;
 }
