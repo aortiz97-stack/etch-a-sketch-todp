4e9a51758c7a94f78bb0295d62422805e69f6d02
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-08 12:23:38 -0600

    Create 16*16 div containers with red background and filler words that help visualize current structure of grid

---
+++ etch-sketch.js
@@ -0,0 +1,9 @@
+const masterContainer = document.getElementsByClassName("master-container")[0];
+
+let count = 0;
+while (count < 16*16){
+    let grid = document.createElement("div");
+    grid.textContent = "Filler Words"
+    masterContainer.appendChild(grid);
+    count ++;
+}
