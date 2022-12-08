d098eaab62e27eb8cb873f84a2fb5d9e7a06144a
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-08 15:34:39 -0600

    Give create new grid button basic functionality, with errors still present with accomodating new grid size

--- etch-sketch.js
+++ etch-sketch.js
@@ -8,9 +8,55 @@ while (count < 16*16){
     count ++;
 }
 
-//Change color of squares
 const nestedDivs = document.querySelectorAll(".master-container div");
-
 nestedDivs.forEach((div) => {
     div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
 });
+
+
+//Create event for button pressing
+const button = document.querySelector("button");
+
+function getDimension(e){
+    if (e!==undefined){
+        let answer = prompt("Please input number n to get a new nxn grid. Number must be less than 100.")
+
+        if (typeof parseInt(answer) !== "number"){
+            throw "Invalid input. You must provide a number."
+        }
+        else if (typeof parseInt(answer) > 100){
+            throw "Invalid input. Input must be less than or equal to 100."
+        }
+        else{
+            return parseInt(answer);
+        }
+    }
+} 
+
+function createNewGrid(e){
+    if (e!==undefined){
+        const gridsToDelete = document.querySelectorAll(".master-container div");
+        const dimension = getDimension(e);
+
+        gridsToDelete.forEach((grid) => {
+            masterContainer.removeChild(grid);
+        });
+
+        let count = 0;
+        while (count < dimension*dimension){
+            let grid = document.createElement("div");
+            masterContainer.appendChild(grid);
+            count ++;
+        }
+
+        //Change color of squares
+        const nestedDivs = document.querySelectorAll(".master-container div");
+
+        nestedDivs.forEach((div) => {
+            div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
+        });
+    }
+}
+
+button.addEventListener("click", createNewGrid)
+
