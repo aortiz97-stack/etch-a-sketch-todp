cdfa39f344254f8ba2550735a54edc7d227c192a
Armando <armando.ortiz.jr.19@outlook.com>
2022-12-09 15:35:33 -0600

    Create first attempt at adjusting padding according to grid dimensions, bugs still present

--- etch-sketch.js
+++ etch-sketch.js
@@ -49,9 +49,13 @@ function createNewGrid(e){
             masterContainer.appendChild(grid);
             count ++;
         }
+        //Set padding of grid squares
+        let gridPadding = getPadding(dimension).toString() +"px";
+        const nestedDivs = document.querySelectorAll(".master-container div");
+        nestedDivs.forEach((div)=>{div.style.padding = gridPadding; console.log(div.style.padding)});
+
 
         //Change color of squares
-        const nestedDivs = document.querySelectorAll(".master-container div");
 
         nestedDivs.forEach((div) => {
             div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
@@ -60,4 +64,32 @@ function createNewGrid(e){
 }
 
 button.addEventListener("click", createNewGrid)
+
+//Calculate padding
+function getPadding(n){
+    let deviceWidth = Math.max(document.documentElement.clientWidth||0, window.innerWidth||0);
+    let sidePadding = 0.6 * deviceWidth;
+    let borderWidth = n;
+    let availableWidth = deviceWidth - sidePadding - borderWidth;
+    console.log(`deviceWidth: ${deviceWidth}`);
+    console.log(`sidePadding: ${sidePadding}`);
+    console.log(`availableWidth: ${availableWidth}`);
+    console.log(`______`);
+
+    let deviceHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    let buttonDivHeight = document.querySelector("button").clientHeight;
+    let bottomPadding = 0.3 * deviceWidth;
+    let availableHeight = deviceHeight - buttonDivHeight - bottomPadding- borderWidth
+
+    console.log(`deviceHEIGHT: ${deviceHeight}`);
+    console.log(`buttonDivHEIGHT: ${buttonDivHeight}`);
+    console.log(`bottomPadding: ${bottomPadding}`);
+    console.log(`availableHeight: ${availableHeight}`);
+    console.log(`______`);
+
+    const deviceDimension = Math.max(availableHeight, availableWidth);
+    console.log(`deviceDimension: ${deviceDimension}`);
+   
+    return deviceDimension/(n*2);
+}
 
