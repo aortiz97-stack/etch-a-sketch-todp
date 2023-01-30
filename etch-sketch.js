23d8c51fcb22c4a8c3b0a7356f374d2c9ca37043
Armando <armando.ortiz.jr.19@outlook.com>
2023-01-30 13:23:10 -0600

    Allow for javascript to adjust grid row, grid column size, and cell padding dynamically when user enters a new dimension

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,89 +1,71 @@
-//Create grid
-const masterContainer = document.getElementsByClassName("master-container")[0];
+/* eslint-disable no-param-reassign */
+/* eslint-disable radix */
+// Create grid
+const masterContainer = document.getElementsByClassName('master-container')[0];
 
 let count = 0;
-while (count < 16*16){
-    let grid = document.createElement("div");
-    masterContainer.appendChild(grid);
-    count ++;
+while (count < 16 * 16) {
+  const grid = document.createElement('div');
+  masterContainer.appendChild(grid);
+  count += 1;
 }
 
-const nestedDivs = document.querySelectorAll(".master-container div");
+const nestedDivs = document.querySelectorAll('.master-container div');
 nestedDivs.forEach((div) => {
-    div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
+  // eslint-disable-next-line no-param-reassign
+  div.addEventListener('mouseover', () => { div.style.backgroundColor = 'black'; });
 });
 
+// Create event for button pressing
+const button = document.querySelector('button');
 
-//Create event for button pressing
-const button = document.querySelector("button");
-
-function getDimension(e){
-    if (e!==undefined){
-        let answer = prompt("Please input number n to get a new nxn grid. Number must be less than 100.")
-        console.log(parseInt(answer));
-        if (typeof parseInt(answer) !== "number" || isNaN(answer)){
-            console.log("enterred")
-            throw "Invalid input. You must provide a number."
-        }
-        else if (parseInt(answer) > 100){
-            throw "Invalid input. Input must be less than or equal to 100."
-        }
-        else{
-            return parseInt(answer);
-        }
+function getDimension(e) {
+  if (e !== undefined) {
+    const answer = prompt('Please input number n to get a new nxn grid. Number must be less than 100.');
+    console.log(parseInt(answer));
+    if (typeof parseInt(answer) !== 'number' || Number.isNaN(answer)) {
+      throw new Error('Invalid input. You must provide a number.');
+    } else if (parseInt(answer) > 100) {
+      throw new Error('Invalid input. Input must be less than or equal to 100.');
+    } else {
+      return parseInt(answer);
     }
-} 
-
-function createNewGrid(e){
-    if (e!==undefined){
-        const gridsToDelete = document.querySelectorAll(".master-container div");
-        const dimension = getDimension(e);
-
-        gridsToDelete.forEach((grid) => {
-            masterContainer.removeChild(grid);
-        });
-
-        let count = 0;
-        while (count < dimension*dimension){
-            let grid = document.createElement("div");
-            masterContainer.appendChild(grid);
-            count ++;
-        }
-        const nestedDivs = document.querySelectorAll(".master-container div");
-
-
-        //Change color of squares
-
-        nestedDivs.forEach((div) => {
-            div.addEventListener("mouseover", ()=>{div.style.backgroundColor = "black"})
-        });
-    }
+  }
+  throw new Error('Event is undefined');
 }
 
-button.addEventListener("click", createNewGrid)
-
-//Calculate padding
-function getPadding(n){
-    let deviceWidth = Math.max(document.documentElement.clientWidth||0, window.innerWidth||0);
-    let sidePadding = 0.6 * deviceWidth;
-    let borderWidth = n;
-    let availableWidth = deviceWidth - sidePadding - borderWidth;
-
-    let deviceHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    let buttonDivHeight = document.querySelector("button").clientHeight;
-    let bottomPadding = 0.3 * deviceWidth;
-    let availableHeight = deviceHeight - buttonDivHeight - bottomPadding- borderWidth
-
-    let availableTotalArea = availableHeight * availableWidth;
-    let deviceArea = deviceWidth * deviceHeight;
-
-    let gridArea = availableTotalArea/n
-
-
-
-    return gridArea/deviceArea * 200;
-    
+// Calculate padding
+function getPaddingPercentage(n) {
+  return (100 - n) / n;
 }
 
-console.log(getPadding(16));
+function createNewGrid(e) {
+  if (e !== undefined) {
+    const gridsToDelete = document.querySelectorAll('.master-container div');
+    const dimension = getDimension(e);
 
+    gridsToDelete.forEach((grid) => {
+      masterContainer.removeChild(grid);
+    });
+
+    masterContainer.style.cssText = `grid-template-rows: repeat(${dimension}, 1fr); grid-template-columns: repeat(${dimension}, 1fr);`;
+
+    let gridCount = 0;
+    while (gridCount < dimension * dimension) {
+      const grid = document.createElement('div');
+      grid.style.cssText = `padding: ${getPaddingPercentage(dimension)}%;`;
+      masterContainer.appendChild(grid);
+      gridCount += 1;
+    }
+    const cngNestedDivs = document.querySelectorAll('.master-container div');
+
+    // Change color of squares
+    cngNestedDivs.forEach((div) => {
+      div.addEventListener('mouseover', () => { div.style.backgroundColor = 'black'; });
+    });
+  } else {
+    throw (new Error('The event is undefined'));
+  }
+}
+
+button.addEventListener('click', createNewGrid);
